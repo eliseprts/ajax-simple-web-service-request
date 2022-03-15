cf7657f6863d5bd738a06c23549f88a75805fff1
eliseprts <elisepourtois.pro@gmail.com>
2022-03-15 12:13:11 +0100

    upload work 15/3 am

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ajax-simple-web-service-request
+# THE HILL | Javascript, season 2
+
+## 01 Ajax
+
+- Publish a web page that shows a random quote from an available Web Service, from the Quotes serving website: https://thatsthespir.it
+- Solo work
+
+### Learning objectives
+
+- A typical Ajax flow : make an asynchronous request to a remote server via javascript and process the response.
+- DOM manipulation : modify the DOM according to the result of the Ajax request.
---
+++ script/script.js
@@ -0,0 +1,34 @@
+
+const quoteElem = document.getElementById('quote');
+
+function getQuote() {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://thatsthespir.it/api`, true);
+
+    xhr.onload = function () {
+
+        if (this.status === 200) {
+
+            const response = JSON.parse(this.responseText);
+            const quote = response.quote;
+            const author = response.author;
+
+            quoteElem.innerHTML = quote + '<br><span class="author"> – ' + author + ' – </span>';
+
+
+        } else {
+
+            quoteElem.innerHTML = 'Erreur'
+
+        }
+
+
+    }
+
+    xhr.send()
+
+}
+
+getQuote();
